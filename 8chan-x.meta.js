a623c7887b227e97400ab5fa2e37088983329ee8
dunkvol4 <72674204+dunkvol4@users.noreply.github.com>
2020-10-10 18:56:59 -0400

    Update 8chan-x.meta.js

--- 8chan-x.meta.js
+++ 8chan-x.meta.js
@@ -1,8 +1,8 @@
 // ==UserScript==
-// @name        Pashe's 8chanX v2 [pure]
-// @version     2.0.0.1511219830
-// @description Small userscript to improve 8chan
-// @namespace   https://github.com/Pashe/8chanX/tree/2-0
+// @name        TvchX
+// @version     3.0
+// @description Small userscript to improve tvch
+// @namespace   https://github.com/dunkvol4/TvchX
 // @updateURL   https://github.com/Pashe/8chanX/raw/2-0_pure/8chan-x.meta.js
-// @downloadURL https://github.com/Pashe/8chanX/raw/2-0_pure/8chan-x.user.js
+// @downloadURL https://github.com/dunkvol4/TvchX/blob/main/Tvch-x.user.js
 // ==/UserScript==
