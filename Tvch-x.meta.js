2bd51d85a41a90f2bb74db820c4ceac2c21cfbf8
dunkvol4 <72674204+dunkvol4@users.noreply.github.com>
2020-10-16 15:51:04 -0400

    Update Tvch-x.meta.js

--- Tvch-x.meta.js
+++ Tvch-x.meta.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name        TvchX
-// @version     3.0.2.1
+// @version     3.0.3
 // @description Small userscript to improve tvch
 // @namespace   https://github.com/dunkvol4/TvchX
 // @updateURL   https://github.com/dunkvol4/TvchX/blob/main/Tvch-x.meta.js
